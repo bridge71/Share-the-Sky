f38600f4a07036856cc43cf2af8daea6af1bbf07
bridge71 <1415806690@qq.com>
2024-01-03 10:38:16 +0800

    finish FileController and update .gitignore

--- controllers/FileController.cc
+++ controllers/FileController.cc
@@ -1,3 +1,103 @@
 #include "FileController.h"
+#include "drogon/orm/Exception.h"
+#include "drogon/orm/Result.h"
 
+using namespace drogon;
+void FileController::Add_File(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> && callback, File file)const{
+    auto dbclient = drogon::app().getDbClient();
+    Json::Value message;
+    try{
+        dbclient->execSqlSync("insert into file(fileName, fileType, MD5, path) values(?, ?, ?, ?)",
+        file.fileName, file.fileType, file.MD5, file.path);
+        message["code"] = "0";
+    }catch(drogon::orm::DrogonDbException &e){
+        message["error"] = "Add failed";
+    }
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(message);
+    callback(resp);
+}
 // Add definition of your processing function here
+
+void FileController::Delete_File(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> && callback, Json::Value json)const{
+    auto dbclient = drogon::app().getDbClient();
+    Json::Value message;
+    int id = json["id"].as<int>();
+    try{
+        dbclient->execSqlSync("delete from file where id = ?", id);
+        message["code"] = 0;
+    }catch (drogon::orm::DrogonDbException &e){
+        message["error"] = "Delete failed";
+    }
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(message);
+    callback(resp);
+}
+void FileController::Find_File_Name(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> && callback, Json::Value json)const{
+    auto dbclient = drogon::app().getDbClient();
+    Json::Value message;
+    try{
+        std::string fileName = json["fileName"].as<std::string>();
+        std::string sql = "select * from file where fileName = ?";
+        std::cout<<sql;
+        auto future = dbclient->execSqlAsyncFuture(sql, fileName);
+        auto result = future.get();
+        for (const auto &row : result){
+            Json::Value item;
+            item["id"] = row["id"].as<int>();
+            item["fileName"] = row["fileName"].as<std::string>();
+            item["fileType"] = row["fileType"].as<std::string>();
+            item["MD5"] = row["MD5"].as<std::string>();
+            item["path"] = row["path"].as<std::string>();
+            message.append(item);
+        }
+    }catch (drogon::orm::DrogonDbException &e){
+        message["error"] = "find file by Name failed";
+    }
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(message);
+    callback(resp);
+}
+void FileController::Find_File_MD5(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> && callback, Json::Value json)const{
+    auto dbclient = drogon::app().getDbClient();
+    Json::Value message;
+    try{
+        std::string MD5 = json["MD5"].as<std::string>();
+        std::string sql = "select * from file where MD5 = ?";
+        auto future = dbclient->execSqlAsyncFuture(sql, MD5);
+        auto result = future.get();
+        for (const auto &row : result){
+            Json::Value item;
+            item["id"] = row["id"].as<int>();
+            item["fileName"] = row["fileName"].as<std::string>();
+            item["fileType"] = row["fileType"].as<std::string>();
+            item["MD5"] = row["MD5"].as<std::string>();
+            item["path"] = row["path"].as<std::string>();
+            message.append(item);
+        }
+    }catch (drogon::orm::DrogonDbException &e){
+        message["error"] = "find file by MD5 failed";
+    }
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(message);
+    callback(resp);
+}
+void FileController::List_File(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> && callback, Json::Value json)const{
+    auto dbclient = drogon::app().getDbClient();
+    Json::Value message;
+    try{
+        std::string path = json["path"].as<std::string>();
+        std::string sql = "select * from file where path = ?";
+        auto future = dbclient->execSqlAsyncFuture(sql, path);
+        auto result = future.get();
+        for (const auto &row : result){
+            Json::Value item;
+            item["id"] = row["id"].as<int>();
+            item["fileName"] = row["fileName"].as<std::string>();
+            item["fileType"] = row["fileType"].as<std::string>();
+            item["MD5"] = row["MD5"].as<std::string>();
+            item["path"] = row["path"].as<std::string>();
+            message.append(item);
+        }
+    }catch (drogon::orm::DrogonDbException &e){
+        message["error"] = "list failed";
+    }
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(message);
+    callback(resp);
+}
--- controllers/FileController.h
+++ controllers/FileController.h
@@ -3,18 +3,84 @@
 #include <drogon/HttpController.h>
 
 using namespace drogon;
-
+struct File{
+  std::string fileName;
+  std::string fileType;
+  std::string MD5;
+  std::string path;
+};
 class FileController : public drogon::HttpController<FileController>
 {
   public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(FileController::get, "/{2}/{1}", Get); // path is /FileController/{arg2}/{arg1}
-    // METHOD_ADD(FileController::your_method_name, "/{1}/{2}/list", Get); // path is /FileController/{arg1}/{arg2}/list
+     METHOD_ADD(FileController::Add_File, "/Add_File", Post); // path is /FileController/{arg1}/{arg2}/list
+     METHOD_ADD(FileController::Delete_File, "/Delete_File", Post); // path is /FileController/{arg1}/{arg2}/list
+     METHOD_ADD(FileController::List_File, "/List_File", Post); // path is /FileController/{arg1}/{arg2}/list
+     METHOD_ADD(FileController::Find_File_Name, "/Find_File_Name", Post); // path is /FileController/{arg1}/{arg2}/list
+     METHOD_ADD(FileController::Find_File_MD5, "/Find_File_MD5", Post); // path is /FileController/{arg1}/{arg2}/list
+
     // ADD_METHOD_TO(FileController::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
 
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
-    // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
+    void Add_File(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, File file) const;
+    void Delete_File(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, Json::Value json) const;
+    void List_File(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, Json::Value json) const;
+    void Find_File_Name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, Json::Value json) const;
+    void Find_File_MD5(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, Json::Value json) const;
+
 };
+namespace drogon{
+  template<>
+  inline File fromRequest(const HttpRequest &req){
+    auto json = req.getJsonObject();
+    File file;
+    if(json == nullptr){
+      throw std::runtime_error("format error, please use json");
+    }
+    if(json->isMember("fileName")){
+      const auto &temp = (*json)["fileName"].asString();
+      if(temp.size() > 100){
+        throw std::runtime_error("the name is too longer");
+      }
+      file.fileName = temp;
+    }else{
+      throw std::runtime_error("name lost");
+    }
+    if(json->isMember("fileType")){
+      const auto &temp = (*json)["fileType"].asString();
+      if(temp.size() > 100){
+        throw std::runtime_error("the type is too longer");
+      }
+      file.fileType = temp;
+    }else{
+      throw std::runtime_error("type lost");
+    }
+    if(json->isMember("MD5")){
+      const auto &temp = (*json)["MD5"].asString();
+      if(temp.size() != 32){
+        throw std::runtime_error("the MD5 is wrong");
+      }
+      file.MD5 = temp;
+    }else{
+      throw std::runtime_error("MD5 lost");
+    }
+    if(json->isMember("path")){
+      const auto &temp = (*json)["path"].asString();
+      if(temp.size() > 100){
+        throw std::runtime_error("the path is too long");
+      }
+      file.path = temp;
+    }else{
+      throw std::runtime_error("path lost");
+    }
+    return file;
+  }
+template<>
+  inline Json::Value fromRequest(const HttpRequest &req){
+    return *req.getJsonObject();
+  }
+}
